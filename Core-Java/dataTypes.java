c989ca463942906f7a754d503072e9e2574f7507
4run-Rangad <arunrangad83@gmail.com>
2025-12-31 21:07:28 +0530

    Second Commit

---
+++ Core-Java/dataTypes.java
@@ -0,0 +1,9 @@
+class dataTypes{
+    public static void main(String[] args){
+    int x = 4,y = 5;
+    System.out.println("Add result: "+ (x+y));
+    System.out.println("Subtract result: "+ (x-y));
+    System.out.println("Product : "+ (x*y));
+    System.out.println("Division : "+ (x/y));
+    }
+}
