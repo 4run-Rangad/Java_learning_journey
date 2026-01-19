ade682fadcb42df8321f5673833777c569451447
4run-Rangad <arunrangad83@gmail.com>
2026-01-19 21:19:19 +0530

    Completed the Bank Account Console App

--- Mini_Projects/BankAccountSystem/main.java
+++ Mini_Projects/BankAccountSystem/main.java
@@ -1,23 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 class BankAccount{
-    private int accNo = -1;
+    private int accNo;
     private String bankname;
     private int balance = 0;
-    BankAccount(){
-    }
     BankAccount(int accNo, String bankname){
         this.accNo = accNo;
         this.bankname = bankname;
-    }
-    public void deposit(int amount){
-        balance+=amount;
-    }
-    public void withdraw(int amount){
-        if(balance<amount) System.out.println("Insufficient Balance!");
-        else balance-=amount;
-    }
-    public void checkBalance(){
-        System.out.println("Total Balance in your account: "+ balance+" rupees");
     }
     public void setaccNo(int n){
         accNo = n;
@@ -25,14 +14,101 @@ class BankAccount{
     public void setbankname(String str){
         bankname = str;
     }
+    public int getAccNo(){
+        return accNo;
+    }
+    public String getName(){
+        return bankname;
+    }
+    public int getBalance(){
+        return balance;
+    }
+    public void setBalance(int amount){
+        balance = amount;
+    }
+    public String toString(){
+        return bankname+ " :("+accNo+") ";
+    }
 }
+class InsufficientBalanceException extends Exception{
+    public InsufficientBalanceException(String str){
+        super(str);
+    }
+}
+class BankService{
+    int bal;
+    ArrayList<BankAccount> accounts = new ArrayList<>();
 
+    //Account Creation
+    public void createAccount(int n , String str){
+        BankAccount b = new BankAccount(n , str);
+        accounts.add(b);
+    }
+
+    //Deposit
+    public void deposit(int accNo , int amount){
+        for(BankAccount b : accounts){
+            if(b.getAccNo()==accNo){
+                b.setBalance(b.getBalance()+amount);
+                System.out.println("Successfully deposited.");
+                return;
+            }
+        }
+        System.out.println("Account not found!");
+    }
+
+    //Withdrawal
+    public void withdraw(int accNo,int amount){
+        for(BankAccount b : accounts){
+            if(b.getAccNo()==accNo){
+                try{
+                    if(b.getBalance()<amount) throw new InsufficientBalanceException("You do not have enough balance to make this withdrawal!");
+                    else{
+                        b.setBalance(b.getBalance()-amount);
+                        System.out.println("Successfull Withdrawal.");
+                        return;
+                    }
+                }
+                catch(InsufficientBalanceException e){
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        System.out.println("Account not found!");
+    }
+
+    //Check Balance
+    public void checkBalance(int accNo){
+        for(BankAccount b : accounts){
+            if(b.getAccNo()==accNo){
+                System.out.println("Total Balance in your account: "+ b.getBalance()+" rupees");
+                return;
+            }
+        }
+        System.out.println("Account not found!");
+    }
+
+    //Display Account Info
+    public void display(int accNo){
+        for(BankAccount b : accounts){
+            if(b.getAccNo()==accNo){
+                System.out.println("Account No.: " + b.getAccNo());
+                System.out.println("Account name: "+ b.getName());
+                return;
+            }
+        }
+        System.out.println("Account not found!");
+    }
+
+}
 public class main{
     public static void main(String[] a){
         
         int option = -1;
-        BankAccount b1 = new BankAccount();      
-        while(option!=5){
+
+        BankService b1 = new BankService();      
+
+        while(option!=6){
         System.out.println("BANK ACCOUNT SYSTEM ");
         System.out.println("***********************");
         System.out.println("How can I help you today....");
@@ -40,7 +116,8 @@ public class main{
         System.out.println("2.Deposit");
         System.out.println("3.Withdraw");
         System.out.println("4.Check Balance");
-        System.out.println("5.Exit");
+        System.out.println("5.Display Account Details");
+        System.out.println("6.Exit");
         Scanner sc = new Scanner(System.in);
         option = sc.nextInt();
             switch(option){
@@ -50,29 +127,39 @@ public class main{
                     sc.nextLine();
                     System.out.println("Enter Bank Name: ");
                     String str = sc.nextLine();
-                    b1.setaccNo(n);
-                    b1.setbankname(str);
+                    b1.createAccount(n , str);
                     break;
                 }
                 case 2:{
+                    System.out.println("Enter the Account Number: ");
+                    int accNo = sc.nextInt();
                     System.out.println("Enter the amount you would like to deposit.");
                     int amount = sc.nextInt();
-                    b1.deposit(amount);
-                    System.out.println("Successfuly deposited.");
+                    b1.deposit(accNo,amount);
                     break;
                 }
                 case 3:{
+                    System.out.println("Enter the Account Number: ");
+                    int accNo = sc.nextInt();
                     System.out.println("Enter the amount you would like to withdraw.");
                     int amount = sc.nextInt();
-                    b1.withdraw(amount);
-                    System.out.println("Successfull Withdrawal.");
+                    b1.withdraw(accNo,amount);
                     break;
                 }
                 case 4:{
-                    b1.checkBalance();
+                    System.out.println("Enter the Account Number: ");
+                    int accNo = sc.nextInt();
+                    b1.checkBalance(accNo);
                     break;
                 }
                 case 5:{
+                    System.out.println("Enter the Account Number: ");
+                    int accNo = sc.nextInt();
+                    b1.display(accNo);
+                    break;
+                }
+                case 6:{
+                    System.out.println("Thank You..");
                     break;
                 }
             }
